1a75cb94191714d67d8ad1530bbf0165ce3132cd
alexjwick <alexjwick03@gmail.com>
2023-01-17 23:57:55 -0500

    markers now able to be added

--- express/main.js
+++ express/main.js
@@ -7,3 +7,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+fetch('./data/stadiums.json')
+    .then((response) => response.json())
+    .then((data) => addToMap(data.stadiums));
+
+function addToMap(stadiumData) {
+    for (var i = 0; i < stadiumData.length; i++) {
+        stadium = stadiumData[i];
+        name = stadium.name;
+        city = stadium.city;
+        latitude = stadium.latitude;
+        longitude = stadium.longitude;
+        if (latitude == 0 && longitude == 0) {
+            break;
+        }
+        console.log(`Adding ${name} at coordinates ${latitude}, ${longitude} in ${city}`);
+        var marker = L.marker([latitude, longitude]).addTo(map);
+    }
+}
